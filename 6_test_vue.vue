6fceb01e842ee4fc79364aa4d41485e0efd481d5
Zhenxingxing <626990784@qq.com>
2018-12-08 18:05:51 +0800

    创建路由

---
+++ 6_test_vue.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		test专用
+	</div>
+</template>
+<script>
+	export default {
+		components: {
+			'my-btn' : MyBtn
+		},
+		data: function () {
+			return {
+				test2: '测试数据'
+			}
+		},
+		methods: {
+			testfunc: function () {
+				console.log('test666')
+			}	
+		}
+	};
+	
+</script>
+<style>
+	
+</style>
